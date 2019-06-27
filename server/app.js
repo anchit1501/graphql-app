e8a7a69ebb11ae3feb417055cbdb41b0a614643e
anchit1501 <1501.anchit@gmail.com>
2019-06-27 20:51:35 +0530

    CORS added to server

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cors
+app.use(cors());
 
 //connect to atlas 
 mongoose.connect('mongodb+srv://master:<password>@cluster0-swr0v.mongodb.net/test?retryWrites=true&w=majority');
