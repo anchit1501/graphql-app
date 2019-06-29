c8f1f5f7a32de466865d4dbec5bdcbe161bd11e4
anchit1501 <1501.anchit@gmail.com>
2019-06-29 23:42:36 +0530

    Add Book Completed

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -19,5 +19,13 @@ const getBooksQuery = gql`
 }
 `;
 
+const addBookMutation= gql`
+mutation($name: String!,$genre: String!, $authorId: ID!){
+    addBook(name:$name,genre:$genre,authorId:$authorId){
+        name
+        id
+    }
+}
+`;
 
-export{getAuthorsQuery,getBooksQuery};
+export{getAuthorsQuery,getBooksQuery,addBookMutation};
